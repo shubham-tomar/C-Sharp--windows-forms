0bd6ae369ea9ad2f78a3be8d5ce4253bf96533c1
Shubham Tomar <31098244+shubham-tomar@users.noreply.github.com>
2020-02-28 13:42:01 +0530

    Add files via upload

---
+++ DC/DashBoard.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DC
+{
+    public partial class DashBoard : Form
+    {
+        public DashBoard()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string aa = textBox1.Text;
+            MessageBox.Show(aa);
+        }
+    }
+}
